6b795067b63f3485170a7f30f3bf4f817858253c
Pallavi Paliwal <pallavipaliwal@Pallavis-MacBook-Pro.local>
2020-01-24 13:19:31 +0530

    Review changes of login

--- src/Mappy/index.js
+++ src/Mappy/index.js
@@ -1,21 +1,14 @@
 
 import './css/login.css';
 
-
-
-
-
-
-
 var i=1;
 
 $(function() {
 
   $("#signupBtn").click(function () {
-    console.log("Hey there !!!!");
-    //var appi = new AppServerApi();
-    //appi.logIn(document.getElementById("usernameTb").value , document.getElementById("password").value);
-   /* $.post("/demo/api/1129/3213e2cfeed268d4ff0e067aa9f5f528d85bdf577e30e3a266f22556865db23a/login",
+    
+    
+   $.post("/demo/api/1129/3213e2cfeed268d4ff0e067aa9f5f528d85bdf577e30e3a266f22556865db23a/login",
       {
         username: document.getElementById("usernameTb").value,
         password: document.getElementById("password").value
@@ -48,7 +41,7 @@ $(function() {
             console.log('Error loading username=' + document.getElementById("usernameTb").value + error);
           }
         });
-      });*/
+      });
   });
 })
 
@@ -58,29 +51,38 @@ function uploadUserData(jsonData, pageNo) {
     return ;
   }
 
-  console.log("user data is going to be add in this function")
+  console.log("user data is going to be add in this function");
+  
 
   document.getElementById("signUpForm").style.display = "none";
   document.getElementById("icon").style.display = "none";
 
 
+  document.getElementById("usersData").classList.add("table-responsive");  
   var table = document.getElementById("usersTable");
-  //table.classList.add("table");
-  //table.classList.add("table-striped");
+  
+  table.classList.add("table");
+  table.classList.add("table-striped");
+  
+  table.style.width = "100%";
   //table.classList.add("table-dark");
 
-  table.setAttribute("border","1");
-  table.setAttribute("cellpadding","20");
-  table.setAttribute("cellspacing","5");
+  //table.setAttribute("border","1");
+  //table.setAttribute("cellpadding","30");
+  //table.setAttribute("cellspacing","5");
 
-  if (pageNo ==1){
+  if (pageNo == 1){
   var header = table.createTHead();
   header.classList.add("thead-dark");
   var row = header.insertRow(0);
-  row.insertCell(0).innerHTML = "Id";
-  row.insertCell(1).innerHTML = "Username";
-  row.insertCell(2).innerHTML = "Balance";
-  row.insertCell(3).innerHTML = "Send"
+  var cell1 = row.insertCell(0).innerHTML = "Id";
+  //cell1.setAttribute("scope","col");
+   cell1 = row.insertCell(1).innerHTML = "Username";
+  //cell1.setAttribute("scope","col");
+   cell1 = row.insertCell(2).innerHTML = "Balance";
+  //cell1.setAttribute("scope","col");
+   cell1 = row.insertCell(3).innerHTML = "Send";
+  //cell1.setAttribute("scope","col");
   }
   let count =i;
   let k=0;
@@ -101,8 +103,8 @@ function uploadUserData(jsonData, pageNo) {
 
   var span = document.createElement('span');
   var buttonDiv = document.getElementById("buttonDiv");
-  buttonDiv.innerHTML = '<button id="prevBtn" value="Previous" >Previous</button>';
-buttonDiv.innerHTML = '<button id="nextBtn" value="Next" >Next</button>';
+ 
+buttonDiv.innerHTML = '<button id="nextBtn" value="Next"  class ="nextBtn" >Next</button>';
 //buttonDiv.onclick = requestNextData(pageNo);
 document.getElementById("nextBtn").addEventListener("click", function(e) {
 	requestNextData(pageNo)
@@ -127,7 +129,7 @@ function requestNextData(pageNo){
       //window.location.replace("/html/users.html");
       //document.getElementById("usersData").innerHTML='<object type="text/html" data="http://localhost:9000/src/html/Users.html" ></object>';
       //userData(jsonData);
-      
+      $("#table_of_items tr").remove(); 
       uploadUserData(jsonData,pageNo);
     },
     error: function (error) {
