7539a0ce0b692c3868838331b8b16fea84b58025
Pallavi975 <59724098+Pallavi975@users.noreply.github.com>
2020-02-17 18:54:51 +0530

    request acknowledgement added to transaction output

---
+++ src/Mappy/js/MappyUiWorkflowCallback.js
@@ -0,0 +1,12 @@
+
+//store response of each workflow according to workflow id.
+class MappyUiWorkflowCallback {
+ setWorkflowId(id){
+  const oThis = this;
+
+ }
+ requestAcknowledgement(id){
+   const oThis = this;
+ }
+}
+export default MappyUiWorkflowCallback;
