ed12420929bf60babf68d432063747b1cd2b7d1e
rachinkapoor <rachin@ost.com>
2020-03-12 07:22:41 +0530

    Update deploy script

--- deployer/deploy.js
+++ deployer/deploy.js
@@ -23,17 +23,12 @@ const exitWithError = (...args) => {
 const validateEnvironmentVariables = () => {
   const envVarlist = {
     "TOKEN_IDS": 'Comma seperated list of tokenIds. No white-spaces. E.g. "1129,1271"',
-    "OST_BROWSER_SDK_BASE_URL": 'Origin of url from where OstWalletSdk js files will be served. E.g. "https://stagingpepocoin.com/"',
     "OST_BROWSER_SDK_VERSION": 'Version of the sdk. Free text. No sapce allowed. E.g. "v-1.0.0" or "v-dev-1"',
-    "OST_BROWSER_SDK_PLATFORM_API_ENDPOINT": 'Ost Platform Api End Point. E.g. "https://api.stagingost.com/testnet/v2/"',
     "OST_BROWSER_SDK_AWS_ACCESS_TOKEN": 'AWS access token to be used to upload files to S3',
     "OST_BROWSER_SDK_AWS_SECRET": 'AWS secret key to be used to upload files to S3',
     "OST_BROWSER_SDK_S3_REGION": 'AWS S3 region'
   };
   const tokenEnvVarList = {
-    "DEMO_MAPPY_UI_API_ENDPOINT_[TOKEN_ID]": "demo-mappy Api EndPoint. E.g. (For tokenId 1129) https://demo-mappy.stagingost.com/demo/api/1129/3213e2cfeed268d4ff0e067aa9f5f528d85bdf577e30e3a266f22556865db23a/",
-    "OST_BROWSER_SDK_IFRAME_ORIGIN_[TOKEN_ID]": "Origin of sdk's index.html. E.g. https://sdk-mappy.stagingpepocoin.com/ if iframe url is https://sdk-mappy.stagingpepocoin.com/[version]/index.html",
-    "DEMO_MAPPY_UI_ORIGIN_[TOKEN_ID]": "Origin of mappy web UI. E.g. https://mappy-client.stagingost.com/",
     "DEMO_MAPPY_UI_S3_PATH_[TOKEN_ID]": "Folder or path in which mappy files are to be uploaded. Not to be confused with bucket."
   };
 
@@ -106,12 +101,6 @@ const main = () => {
   const sdkVersion = process.env.OST_BROWSER_SDK_VERSION;
   const SOURCE_FOLDER = Path.resolve(__dirname, `../dist/${sdkVersion}/`);
 
-  let sdkDomain = process.env.OST_BROWSER_SDK_BASE_URL;
-  sdkDomain = sdkDomain.trimRight('/');
-
-  let frameSrcPrefix = sdkDomain.replace('https://', "https://*.");
-  frameSrcPrefix = frameSrcPrefix.trimRight('/');
-
 
   let config = {
     "buildLocation": Path.resolve(`../dist/${sdkVersion}/`),
@@ -136,15 +125,7 @@ const main = () => {
       "bucket": SDK_BUCKET,
       "s3Path": `/${sdkVersion}/`,
       "uploadOptions": {
-        "CacheControl": `${CC_MAX_AGE}`,
-        "ContentSecurityPolicy": [
-          "default-src 'none'",
-          `base-uri 'none'`,
-          `block-all-mixed-content`,
-          `connect-src ${process.env.OST_BROWSER_SDK_PLATFORM_API_ENDPOINT}`,
-          `script-src ${sdkDomain}/${sdkVersion}/ost-sdk-iframe-script.js`,
-          `frame-src ${frameSrcPrefix}/${sdkVersion}/index.html`
-        ].join('; ')
+        "CacheControl": `${CC_MAX_AGE}`
       }
     },
     
@@ -153,13 +134,7 @@ const main = () => {
       "bucket": KM_BUCKET,
       "s3Path": `/${sdkVersion}/`,
       "uploadOptions": {
-        "CacheControl": `${CC_MAX_AGE}`,
-        "ContentSecurityPolicy": [
-          "default-src 'none'",
-          `base-uri 'none'`,
-          `block-all-mixed-content`,
-          `script-src ${sdkDomain}/${sdkVersion}/ost-sdk-key-manager-script.js`,
-        ].join('; ')
+        "CacheControl": `${CC_MAX_AGE}`
       }
     },
 
